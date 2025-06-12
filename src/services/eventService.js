148aee952420be0ade63638b400a6b0c7b240cb4
imnotmatheus <matheus.f.reguta@gmail.com>
2025-06-12 20:04:38 -0300

    [fix] agrego validacion de objectId en eventService

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -2,22 +2,22 @@ import { findUserEvents } from "../data/eventData.js";
 import { ObjectId } from "mongodb";
 
 export const getUserEvents = async (userId) => {
-	try {
-		if (!userId) {
-			const error = new Error("User ID is required");
-			error.status = 400;
-			throw error;
-		}
+  try {
+    if (!userId || !ObjectId.isValid(userId)) {
+      const error = new Error("User ID is invalid or required");
+      error.status = 400;
+      throw error;
+    }
 
-		const objectId = new ObjectId(userId);
-		return await findUserEvents(objectId);
-	} catch (error) {
-		if (error.status) {
-			throw error;
-		}
-		throw {
-			message: "Internal server error",
-			status: 500,
-		};
-	}
+    const objectId = new ObjectId(userId);
+    return await findUserEvents(objectId);
+  } catch (error) {
+    if (error.status) {
+      throw error;
+    }
+    throw {
+      message: "Internal server error",
+      status: 500,
+    };
+  }
 };

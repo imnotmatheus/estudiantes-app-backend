6c76dc3e9540b4a5146a3877ca5fd21a61d38b2e
LicFuraca <martinexequield@gmail.com>
2025-06-12 22:30:54 +0000

    fix how we were getting the endpoint query param

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -2,7 +2,7 @@ import { getUserEvents } from "../services/eventService.js";
 
 export const getUserEventsController = async (req, res) => {
 	try {
-		const events = await getUserEvents(req.user.id);
+		const events = await getUserEvents(req.query.id);
 		res.status(200).json(events);
 	} catch (error) {
 		const status = error.status || 500;

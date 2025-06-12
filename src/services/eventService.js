8b21c3461f840aaa6546d90878318fcd703c61a1
lolowaj <lorenawaj@gmail.com>
2025-06-12 20:41:43 -0300

    Agrego validaciones a la capa Service

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,19 +1,35 @@
+import { ObjectId } from "mongodb";
 import { findEventById } from "../data/eventData.js";
 
 export const searchEventById = async (id) => {
     if (!id) {
-        const error = new Error("Es requerido el ID del Evento");
+        const error = new Error("Event ID is required");
         error.statusCode = 400;
         throw error;
     }
 
-    const event = await findEventById(id);
-
-    if (!event) {
-        const error = new Error("Evento no encontrado");
-        error.statusCode = 404;
+    if (!ObjectId.isValid(id)) {
+        const error = new Error("Invalid ID format");
+        error.statusCode = 400;
         throw error;
     }
 
-    return event;
+    try {
+        const event = await findEventById(id);
+
+        if (!event) {
+            const error = new Error("Event not found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        return event;
+    } catch (error) {
+        if (error.statusCode) {
+            throw error;
+        }
+
+        error.statusCode = 500;
+        throw error;
+    }
 };
